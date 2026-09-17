450f28238349e97859917f52d27cf4fd7de7cfac
aaruffini <aaruffini@gmail.com>
2026-09-17 18:16:40 -0400

    added basic connection.cs to test for a basic connection to the plc

---
+++ basic_connection.cs
@@ -0,0 +1,18 @@
+// Example tag configuration for a global DINT tag in an Allen-Bradley CompactLogix/ControlLogix PLC
+var myTag = new TagDint()
+{
+    Name = "SomeDINT",
+    Gateway = "10.10.10.10",
+    Path = "1,0",
+    PlcType = PlcType.ControlLogix,
+    Protocol = Protocol.ab_eip
+};
+
+// Read the value from the PLC and output to console
+int output = myTag.Read();
+Console.WriteLine($"Original value: {output}");
+
+// Write a new value to the PLC, then read it back, and output to console
+myTag.Write(37);
+output = myTag.Read();
+Console.WriteLine($"Updated value: {output}");
